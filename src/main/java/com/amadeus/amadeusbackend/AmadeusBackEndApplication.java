01d7a78cd0392f3ad50e40e8f9be79aa44ec3e51
Daniela Castro <dcastrog1@eafit.edu.co>
2024-11-06 15:46:15 -0500

    First commit

---
+++ src/main/java/com/amadeus/amadeusbackend/AmadeusBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.amadeus.amadeusbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmadeusBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmadeusBackEndApplication.class, args);
+	}
+
+}
