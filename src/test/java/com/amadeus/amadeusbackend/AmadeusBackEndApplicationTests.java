1ff3acccbacbce3b1b639d376d914fbee34e8e3d
Daniela Castro <dcastrog1@eafit.edu.co>
2024-11-06 15:46:15 -0500

    First commit

---
+++ src/test/java/com/amadeus/amadeusbackend/AmadeusBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amadeus.amadeusbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmadeusBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
